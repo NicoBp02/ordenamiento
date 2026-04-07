9d36ede443530fb6928af289cd3df7454141e598
Nicolas <est.nicolasa.beltran@unimilitar.edu.co>
2026-04-07 09:55:40 -0500

    Se implementa bucle para captura de datos del usuario

--- ordenamiento/src/main/java/com/proyecto/ordenamiento/Ordenamiento.java
+++ ordenamiento/src/main/java/com/proyecto/ordenamiento/Ordenamiento.java
@@ -14,7 +14,10 @@ public class Ordenamiento {
         int n = sc.nextInt();
         int[] arr = new int[n];
         ordenar(arr);
-
+ for (int i = 0; i < n; i++) {
+            System.out.print("Ingresa el valor para la posicion " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
     }
 
     
